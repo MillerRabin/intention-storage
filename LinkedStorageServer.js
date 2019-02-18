1595dbbd5fe05af7b8c646b615ecfb9c0a9d4e3d
MillerRabin <millerrabin@raintech.su>
2019-02-19 01:11:12 +0300

    added translate intention

--- LinkedStorageServer.js
+++ LinkedStorageServer.js
@@ -5,7 +5,7 @@ module.exports = class LinkedStorageServer extends LinkedStorageAbstract {
     constructor({ storage, port = 10010 }) {
         super({ storage, port });
         const wss = new WebSocket.Server({ port: port });
-        wss.on('connection', function (ws) {
+        wss.on('connection', (ws) => {
             this.socket = ws;
         });
     }
